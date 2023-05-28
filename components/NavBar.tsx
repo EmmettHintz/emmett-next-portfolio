3c7c42aa1d061829806b08242cb207b372098df2
EmmettHintz <eahintz@colgate.edu>
2023-05-27 23:35:10 -0400

    updated nav bar and profile picture

--- components/NavBar.tsx
+++ components/NavBar.tsx
@@ -1,16 +1,24 @@
-import Link from "next/link";
+import Link from 'next/link';
 
 const NavBar = () => {
   return (
-    <ul className="py-4 flex justify-between  ">
+    <ul className="py-4 flex justify-between">
       <li>
-        <Link className="rounded px-4 py-2 underline text-red-400" href="/">
-          Home
+        <Link href="/">
+          <span className="rounded px-4 py-2 cursor-pointer text-red-400 hover:text-red-600 transition-colors duration-200">
+            <span className="border-b-2 border-transparent hover:border-red-400 transition-all duration-200">
+              Home
+            </span>
+          </span>
         </Link>
       </li>
       <li className="mr-4">
-        <Link className=" rounded px-4 py-2 text-red-400 gap-4 underline" href="/resume">
-          Contact
+        <Link href="/resume">
+          <span className="rounded px-4 py-2 cursor-pointer text-red-400 hover:text-red-600 transition-colors duration-200">
+            <span className="border-b-2 border-transparent hover:border-red-400 transition-all duration-200">
+              Contact
+            </span>
+          </span>
         </Link>
       </li>
     </ul>
