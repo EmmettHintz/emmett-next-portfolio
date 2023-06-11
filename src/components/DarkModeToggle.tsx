afe3e821cbb27d07fefcab88ac9f92bcce221ca1
EmmettHintz <eahintz@colgate.edu>
2023-06-11 18:28:57 -0400

    updated button styling

--- src/components/DarkModeToggle.tsx
+++ src/components/DarkModeToggle.tsx
@@ -26,13 +26,13 @@ const DarkModeToggle: React.FC = () => {
 
   return (
     <button
-      className="fixed right-4 top-4 h-12 w-12 rounded border border-gray-800 bg-white dark:border-white dark:bg-gray-800"
+      className="fixed right-4 top-4 h-12 w-12 rounded border border-gray-800 bg-white dark:border-white dark:bg-gray-800 flex items-center justify-center sm:right-6 sm:top-6 sm:h-16 sm:w-16"
       onClick={() => setDarkMode(!darkMode)}
     >
       {darkMode ? (
-        <FontAwesomeIcon icon={faSun} className="h-6 text-center w-6 text-yellow-500" />
+        <FontAwesomeIcon icon={faSun} className="h-6 w-6 text-yellow-500" />
       ) : (
-        <FontAwesomeIcon icon={faMoon} className="h-6 w-6 text-center text-gray-800" />
+        <FontAwesomeIcon icon={faMoon} className="h-6 w-6 text-gray-800" />
       )}
     </button>
   );
