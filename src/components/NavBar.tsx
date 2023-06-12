068c0c88968ed1d00721b1190e58bbc7948e89ae
EmmettHintz <eahintz@colgate.edu>
2023-06-12 13:09:56 -0400

    udpated nav bar responsiveness

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -7,27 +7,27 @@ const NavBar = () => {
 
       <DarkModeToggle />
 
-      <div className="text-secondary-heading text-xl">
-        <ul className="nav-links flex  items-center justify-items-center py-4">
-          <li className="pr-4">
+      <div className="text-secondary-heading sm:text-md text-xl">
+        <ul className="nav-links flex items-center justify-items-center py-4">
+          <li className="pr-4 sm:pr-2">
             <Link href="/">
               <span className="hover:underline transition-all duration-300">Home</span>
             </Link>
           </li>
 
-          <li className="pr-4">
+          <li className="pr-4 sm:pr-2">
             <Link href="/resume">
               <span className="hover:underline transition-all duration-300">Resume</span>
             </Link>
           </li>
 
-          <li className="pr-4">
+          <li className="pr-4 sm:pr-2">
             <Link href="/projects">
               <span className="hover:underline transition-all duration-300">Projects</span>
             </Link>
           </li>
 
-          <li className="pr-4">
+          <li className="pr-4 sm:pr-2">
             <Link href="/contact">
               <span className="hover:underline transition-all duration-300">Contact </span>
             </Link>
