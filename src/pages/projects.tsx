11fcf76970c5b593326ff1daedd69317ee93aaee
EmmettHintz <eahintz@colgate.edu>
2023-06-12 11:29:59 -0400

    added content to projects

--- src/pages/projects.tsx
+++ src/pages/projects.tsx
@@ -1,30 +1,30 @@
 import Link from "next/link";
 import NavBar from "~/components/NavBar";
-import { useTheme } from 'next-themes'
+import { useTheme } from "next-themes";
 
 const Projects = () => {
   return (
-    <main className="min-h-screen bg-white dark:bg-bg-dark px-4 sm:px-20">
+    <main className="min-h-screen bg-white px-4 dark:bg-bg-dark sm:px-20">
       <div>
         <NavBar />
       </div>
 
       <div className="flex justify-center py-4">
-        <h2 className="text-6xl font-bold text-primary-heading dark:text-primary-heading transition-all duration-500 sm:text-6xl md:text-8xl">
+        <h2 className="text-6xl font-bold text-primary-heading transition-all duration-500 dark:text-primary-heading sm:text-6xl md:text-8xl">
           Projects
         </h2>
       </div>
 
       <div className="flex justify-center py-8">
-        <p className="text-lg text-text-light dark:text-text-dark text-center sm:text-xl md:text-3xl">
+        <p className="text-center text-lg text-tertiary-heading sm:text-xl md:text-3xl">
           This summer, I am working on a few areas that currently interest me
           and I will be posting updates here.
         </p>
       </div>
 
       <div className="grid h-auto grid-cols-1 gap-x-7 py-8 md:grid-cols-2">
-        <div className="flex flex-col justify-center items-center gap-y-1 text-text-light dark:text-text-dark text-center">
-          <p className="text-2xl font-semibold text-primary-heading dark:text-primary-heading sm:text-4xl md:text-6xl">
+        <div className="flex flex-col items-center justify-center gap-y-1 text-center text-text-light dark:text-text-dark">
+          <p className="text-2xl pb-4 font-semibold text-primary-heading dark:text-primary-heading sm:text-4xl md:text-6xl">
             Full-Stack Development
           </p>
 
@@ -35,16 +35,39 @@ const Projects = () => {
           </p>
         </div>
 
-        <div className="flex flex-col justify-center items-center gap-y-1 text-text-light dark:text-text-dark text-center">
+        <div className="flex flex-col items-center justify-center gap-y-1 text-center text-text-light dark:text-text-dark">
           <p className="py-4 text-2xl font-bold text-secondary-heading dark:text-secondary-heading sm:text-4xl md:text-6xl">
             Present project:
           </p>
 
           <Link
             href="https://litlocator.com/"
-            className="text-primary-heading dark:text-primary-heading text-2xl hover:text-pink-500 hover:underline sm:text-4xl md:text-6xl transition-all duration-500"
+            className="text-2xl text-primary-heading transition-all duration-500 hover:text-pink-500 hover:underline dark:text-primary-heading sm:text-4xl md:text-6xl"
           >
             LitLocator
+          </Link>
+        </div>
+
+        <div className="flex flex-col pt-16 items-center justify-center gap-y-1 py-8 text-center text-text-light dark:text-text-dark">
+          <p className="text-2xl pb-4 font-semibold text-primary-heading dark:text-primary-heading sm:text-4xl md:text-6xl">
+            Machine Learning + Fast.AI
+          </p>
+
+          <p className="text-lg sm:text-xl md:text-3xl">
+            I am following along with fast.ai's Practical Deep Learning for Coders course and textbook to learn about neural networks and deep learning. I want to learn to deploy apps using these ideas and apply my web-development skills towards HCI.
+          </p>
+        </div>
+
+        <div className="flex flex-col py-4items-center justify-center gap-y-1 text-center text-text-light dark:text-text-dark">
+          <p className="text-2xl font-bold pb-4 text-secondary-heading dark:text-secondary-heading sm:text-4xl md:text-6xl">
+            Present project:
+          </p>
+
+          <Link
+            href="https://course.fast.ai/"
+            className="text-2xl text-primary-heading transition-all duration-500 hover:text-pink-500 hover:underline dark:text-primary-heading sm:text-4xl md:text-6xl"
+          >
+            fast.ai
           </Link>
         </div>
       </div>
