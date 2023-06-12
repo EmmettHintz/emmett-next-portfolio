e265b68139626b556783e3da18e5aaa96e2a5c8e
EmmettHintz <eahintz@colgate.edu>
2023-06-12 12:53:55 -0400

    fixed weird error

--- src/components/DarkModeToggle.tsx
+++ src/components/DarkModeToggle.tsx
@@ -1,12 +1,12 @@
-import { useEffect, useState } from 'react';
+import { useEffect, useState } from "react";
 import { faSun, faMoon } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { useTheme } from 'next-themes'
+import { useTheme } from "next-themes";
 
 const DarkModeToggle: React.FC = () => {
   const { theme, setTheme } = useTheme();
   const [mounted, setMounted] = useState(false);
-  const isDark = theme === 'dark';
+  const isDark = theme === "dark";
 
   useEffect(() => {
     // Component did mount
@@ -14,7 +14,7 @@ const DarkModeToggle: React.FC = () => {
   }, []);
 
   const toggleTheme = () => {
-    setTheme(isDark ? 'light' : 'dark');
+    setTheme(isDark ? "light" : "dark");
   };
 
   // Avoid rendering the component on the server
@@ -24,7 +24,7 @@ const DarkModeToggle: React.FC = () => {
 
   return (
     <button
-      className="absolute right-4 top-4 h-12 w-12 rounded  border border-gray-800 bg-white dark:border-gray-400 dark:bg-gray-800 flex items-center justify-center sm:right-6 sm:top-6 sm:h-16 sm:w-16"
+      className="absolute right-4 top-4 flex h-12 w-12  items-center justify-center rounded border border-gray-800 bg-white dark:border-gray-400 dark:bg-gray-800 sm:right-6 sm:top-6 sm:h-16 sm:w-16"
       onClick={toggleTheme}
     >
       {isDark ? (
@@ -37,4 +37,3 @@ const DarkModeToggle: React.FC = () => {
 };
 
 export default DarkModeToggle;
-``
