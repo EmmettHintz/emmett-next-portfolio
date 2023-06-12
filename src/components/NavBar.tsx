41682525a27e9f0fdb406ed09488a8abff544201
EmmettHintz <eahintz@colgate.edu>
2023-06-12 15:12:50 -0400

    updated mobile responsiveness

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -7,27 +7,27 @@ const NavBar = () => {
 
       <DarkModeToggle />
 
-      <div className="text-secondary-heading sm:text-md text-xl">
-        <ul className="nav-links flex items-center justify-items-center py-4">
-          <li className="pr-4 sm:pr-2">
+      <div className="text-secondary-heading sm:text-md text-sm sm:text-xl">
+        <ul className="nav-links flex flex-col sm:flex-row items-center justify-items-center py-4">
+          <li className="pr-4 sm:pr-2 pb-2 sm:pb-0">
             <Link href="/">
               <span className="hover:underline transition-all duration-300">Home</span>
             </Link>
           </li>
 
-          <li className="pr-4 sm:pr-2">
+          <li className="pr-4 sm:pr-2 pb-2 sm:pb-0">
             <Link href="/resume">
               <span className="hover:underline transition-all duration-300">Resume</span>
             </Link>
           </li>
 
-          <li className="pr-4 sm:pr-2">
+          <li className="pr-4 sm:pr-2 pb-2 sm:pb-0">
             <Link href="/projects">
               <span className="hover:underline transition-all duration-300">Projects</span>
             </Link>
           </li>
 
-          <li className="pr-4 sm:pr-2">
+          <li className="pr-4 sm:pr-2 pb-2 sm:pb-0">
             <Link href="/contact">
               <span className="hover:underline transition-all duration-300">Contact </span>
             </Link>
