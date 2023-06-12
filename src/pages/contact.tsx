a8b67c027570645ca80979fb1db152ce9681c877
EmmettHintz <eahintz@colgate.edu>
2023-06-12 12:50:01 -0400

    fixed contact page

--- src/pages/contact.tsx
+++ src/pages/contact.tsx
@@ -1,7 +1,7 @@
+import React from "react";
 import NavBar from "~/components/NavBar";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faLinkedin, faGithub } from "@fortawesome/free-brands-svg-icons";
 import Link from "next/link";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 const Contact = () => {
   return (
@@ -11,31 +11,34 @@ const Contact = () => {
       </div>
 
       <div className="flex justify-center">
-        <h2 className="text-6xl font-extrabold pb-16 text-primary-heading">Contact Me!</h2>
+        <h2 className="pb-16 text-9xl font-extrabold text-primary-heading">
+          Contact Me!
+        </h2>
       </div>
 
-      <div className="inline-flex justify-center text-center w-full gap-4">
-        <p className="text-4xl font-medium text-tertiary-heading">Email: {""}</p>
-        <p className="text-4xl font-medium text-secondary-heading">eahintz@colgate.edu</p>
-      </div>
+      <div className="flex w-full text-6xl gap-y-12 flex-col justify-center gap-4 text-center">
+        <div className="inline-flex gap-2 justify-center">
+          <p className=" font-medium text-tertiary-heading">
+            Email: {""}
+          </p>
+          <p className="font-medium text-secondary-heading">
+            eahintz@colgate.edu
+          </p>
+        </div>
 
-      <div className="grid grid-cols-2">
-        <div className="flex items-center justify-center">
-          <Link
-            href="http://www.linkedin.com/in/emmett-hintz/"
-            className="text-[30vw] text-blue-600 sm:text-[20vw] md:text-[15vw] lg:text-[25vw] rounded-full hover:opacity-80  hover:scale-105 transition-all duration-300"
-          >
-            <FontAwesomeIcon icon={faLinkedin} />
-          </Link>
-        </div>
-        <div className="flex items-center justify-center">
-          <Link
-            href="https://github.com/EmmettHintz"
-            className="text-[30vw] text-bg-dark dark:text-white sm:text-[20vw] md:text-[15vw] lg:text-[25vw] rounded-full hover:opacity-80  hover:scale-105 transition-all duration-300"
-          >
-            <FontAwesomeIcon icon={faGithub} />
-          </Link>
-        </div>
+        <Link
+          href="http://www.linkedin.com/in/emmett-hintz/"
+          className="text-6xl font-medium text-blue-600 hover:underline"
+        >
+          Linkedin
+        </Link>
+
+        <Link
+          href="https://github.com/EmmettHintz"
+          className="text-6xl font-medium text-bg-dark dark:text-text-dark hover:underline"
+        >
+          GitHub
+        </Link>
       </div>
     </main>
   );
